5e4e878c419025069ee3d9bdaf1c0cd82afd6b6c
Eduardo Quirino <edu.3532@gmail.com>
2022-05-12 16:53:20 -0300

    Adicionado função adicionar contatos e integração com banco

--- ControleDeContatos/Controllers/ContatoController.cs
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace ControleDeContatos.Controllers
 {
     public class ContatoController : Controller
     {
+        private readonly IContatoRepositorio _contatoRepositorio;
+        public ContatoController(IContatoRepositorio contatoRepositorio)
+        {
+            _contatoRepositorio = contatoRepositorio;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<ContatoModel> contatos= _contatoRepositorio.BuscarTodos();
+            return View(contatos);
         }
         public IActionResult Criar()
         {
@@ -21,6 +32,11 @@ namespace ControleDeContatos.Controllers
             return View();
         }
 
-
+        [HttpPost]
+        public IActionResult Criar(ContatoModel contato)
+        {
+            _contatoRepositorio.Adicionar(contato);
+            return RedirectToActionPermanent("Index");
+        }
     }
 }
--- README.md
+++ README.md
@@ -5,4 +5,15 @@
  [Criando um Sistema do Zero - ASP.NET MVC](https://www.youtube.com/watch?v=-v0sfER0po8&list=PLJ0IKu7KZpCQKdwRbU7HfXW3raImmghWZ) " <> Vídeo - 01 12:58 / 41:54"
 
 > - Vídeo - 01 Criando o Front-End
+> - - 02 Cadastro de Contatos - EF SQL Server
 
+> **Configurações do Servidor Local**
+>
+> Nome do Servidor : DESKTOP-NJBDSHG\\SQLEXPRESS
+>
+> Tipo de autenticação windows: Trusted_Connection = yes
+
+> **Rodar migração:**
+>
+> - Add-Migration CriandoNomedaTabela -Context BancoContext 
+> -  Update-Database -Context BancoContext
